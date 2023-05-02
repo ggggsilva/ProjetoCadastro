c7d2b9303473319fb4b38d00f00e5a2d3ee1032f
GABRIEL <gabriel_2018@outlook.com>
2023-05-02 09:35:36 -0300

    BANCO1

---
+++ Banco/Banco.sql
@@ -0,0 +1,15 @@
+-- Arquivo de criação de banco de dados
+DROP DATABASE IF EXISTS projeto1;
+CREATE DATABASE projeto1;
+USE projeto1;
+
+CREATE TABLE usuario
+(
+ id_usuario int(4) PRIMARY KEY auto_increment,
+ nome_usuario  varchar(50) NOT NULL,
+ email_usuario varchar(50) NOT NULL,
+ fone_usurio   varchar(30) NULL,
+ senha         varchar(30) NOT NULL
+
+);
+       
---
+++ Banco/revisao.sql
@@ -0,0 +1,17 @@
+-- Arquivo de criação
+-- do banco de dados
+
+-- COMANDOS SQL SE DIVIDEM EKM DUAS LINGUAGENS
+-- DDL(data Definition Languagem)
+-- Comandos de definição do banco de dados
+-- Ex: CREATE DATABASE NAME
+--     DROP DATABASE NAME
+--   CREATE TABLE NAME(COLUNAS)
+--      DROP TABLE NAME
+-- --------------------------------------------
+--  DML (DATA MANIPULATION LANGUAGE)
+-- == COMANDOS DE MANIPULAÇAO DE DADOS
+-- eX: SELECT COLUNAS FROM TABELA
+--     INSERT INTO TABELA (COLUNAS) VALUES (VALORES)
+--     DELETE FROM TABELA
+--     UPDATE COLUNAS FROM TABELA
